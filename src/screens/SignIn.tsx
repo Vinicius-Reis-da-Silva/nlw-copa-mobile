ab472c8ac0a2d850bf7c28e9c7a1c37863b969a4
Vinicius-Reis-da-Silva <viniciusreissilva808@gmail.com>
2022-11-06 00:15:35 -0300

    First Commiting in mobile

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import { Text, Center } from 'native-base';
+
+export function Signln() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+        <Text color="white" fontSize={24} fontFamily="heading">
+            Hello World Signln!
+        </Text>
+    </Center>
+  );
+}
